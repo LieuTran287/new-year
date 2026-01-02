8184473faee74019a74565319f2fc71173b32377
Tran Lieu <tranlieu@yopmail.com>
2026-01-03 00:11:20 +0700

    update something

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
+const repoName = "new-year"; // tên repo GitHub
+
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
   reactStrictMode: true,
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
   output: 'export',
   images: {
     unoptimized: true,
--- out/_next/static/chunks/b29575b8b91b9d5d.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_error", [
-  "static/chunks/6276a3fe71c28a8d.js",
-  "static/chunks/10db30687cfe5bfc.js",
-  "static/chunks/turbopack-8040a6603b8d5df5.js"
-])
