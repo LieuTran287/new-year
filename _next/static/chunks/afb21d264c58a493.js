171f9e2da7e463140f4640c1ff71a07de015a974
Tran Lieu <tranlieu@yopmail.com>
2026-01-03 00:05:06 +0700

    Updates

--- _next/static/chunks/afb21d264c58a493.js
+++
@@ -1,6 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/8be058dacda94659.js",
-  "static/chunks/10db30687cfe5bfc.js",
-  "static/chunks/bab23cea39425515.css",
-  "static/chunks/turbopack-2d7667dab56a4e9f.js"
-])
--- _next/static/chunks/e22ea403005c50df.js
+++
@@ -1,6 +0,0 @@
-__turbopack_load_page_chunks__("/", [
-  "static/chunks/feecf3c22e25373f.js",
-  "static/chunks/10db30687cfe5bfc.js",
-  "static/chunks/5bafc6d1a79233e2.css",
-  "static/chunks/turbopack-134d40d68fa84d2b.js"
-])
